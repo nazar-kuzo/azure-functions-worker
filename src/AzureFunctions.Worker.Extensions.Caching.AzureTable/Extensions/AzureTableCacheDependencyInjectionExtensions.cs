50d8c600bf80a5cedabdb658672812d6683bd522
Kuzo Nazar <nazar.kuzo@outlook.com>
2025-01-23 12:29:48 +0200

    updated cache registaration api

--- src/AzureFunctions.Worker.Extensions.Caching.AzureTable/Extensions/AzureTableCacheDependencyInjectionExtensions.cs
+++ src/AzureFunctions.Worker.Extensions.Caching.AzureTable/Extensions/AzureTableCacheDependencyInjectionExtensions.cs
@@ -6,22 +6,45 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class AzureTableCacheDependencyInjectionExtensions
 {
+    /// <summary>
+    /// Adds Azure Table Storage as a distributed cache.
+    /// </summary>
+    /// <param name="worker">Functions application builder</param>
+    /// <param name="configureCacheOptions">Optional post configure options bound from `AzureTableCache` config section</param>
+    /// <returns>Functions application builder for chaining</returns>
     public static FunctionsApplicationBuilder AddAzureTableCache(
         this FunctionsApplicationBuilder worker,
         Action<AzureTableCacheOptions>? configureCacheOptions = null)
     {
-        worker.Services
+        worker.Services.AddAzureTableCache(worker.Configuration, configureCacheOptions);
+
+        return worker;
+    }
+
+    /// <summary>
+    /// Adds Azure Table Storage as a distributed cache.
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="configuration">Configuration to bind `AzureTableCache` section</param>
+    /// <param name="configureCacheOptions">Optional post configure options</param>
+    /// <returns>Service collection for chaining</returns>
+    public static IServiceCollection AddAzureTableCache(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        Action<AzureTableCacheOptions>? configureCacheOptions = null)
+    {
+        services
             .AddSingleton<IDistributedCache, AzureTableCache>()
             .Configure<AzureTableCacheOptions>(cacheOptions =>
             {
-                worker.Configuration.GetSection("AzureTableCache").Bind(cacheOptions);
+                configuration.GetSection("AzureTableCache").Bind(cacheOptions);
 
                 configureCacheOptions?.Invoke(cacheOptions);
 
                 cacheOptions.ApplicationName ??= "Default";
-                cacheOptions.ConnectionString ??= worker.Configuration.GetValue<string>("AzureWebJobsStorage")!;
+                cacheOptions.ConnectionString ??= configuration.GetValue<string>("AzureWebJobsStorage")!;
             });
 
-        return worker;
+        return services;
     }
 }

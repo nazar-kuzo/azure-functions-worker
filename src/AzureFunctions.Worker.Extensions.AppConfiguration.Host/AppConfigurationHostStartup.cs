bbb2cc303df0ccc56a63f7d6bdf9171c7bbb4914
Kuzo Nazar <nazar.kuzo@clarustech.com>
2025-08-06 20:00:21 +0300

    replaced existing name resolver with ability to resolve nested settings; allowed configuration binding outside "Values" scope in local.settings.json

--- src/AzureFunctions.Worker.Extensions.AppConfiguration.Host/AppConfigurationHostStartup.cs
+++ src/AzureFunctions.Worker.Extensions.AppConfiguration.Host/AppConfigurationHostStartup.cs
@@ -3,21 +3,18 @@ using AzureFunctions.Worker.Extensions.AppConfiguration.Host;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 [assembly: WebJobsStartup(typeof(AppConfigurationHostStartup))]
 
 namespace AzureFunctions.Worker.Extensions.AppConfiguration.Host;
 
-public class AppConfigurationHostStartup : IWebJobsConfigurationStartup
+public class AppConfigurationHostStartup : IWebJobsStartup, IWebJobsConfigurationStartup
 {
     public void Configure(WebJobsBuilderContext context, IWebJobsConfigurationBuilder builder)
     {
-        if (!IsAppConfigurationEnabled())
-        {
-            return;
-        }
-
-        if (Environment.GetEnvironmentVariable("APPCONFIG_ENDPOINT") is string appConfigEndpoint)
+        if (TryGetAppConfigurationEndpoint() is string appConfigEndpoint)
         {
             // TODO: add option for credentials local cache to improve performance
             var credentials = new DefaultAzureCredential(new DefaultAzureCredentialOptions
@@ -41,19 +38,27 @@ public class AppConfigurationHostStartup : IWebJobsConfigurationStartup
             ChangeAppConfigurationSourcePriority(builder.ConfigurationBuilder.Sources);
         }
 
-        static bool IsAppConfigurationEnabled()
+        if (IsDevelopment())
         {
-            if (Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "Development")
+            // allows configuration binding outside "Values" scope in local.settings.json
+            builder.ConfigurationBuilder.AddJsonFile(
+                new PhysicalFileProvider(context.ApplicationRootPath),
+                path: "local.settings.json",
+                optional: true,
+                reloadOnChange: true);
+        }
+
+        static string? TryGetAppConfigurationEndpoint()
+        {
+            if (IsDevelopment() ||
+                (Environment.GetEnvironmentVariable("APPCONFIG_EXTENSION_ENABLED") is string extensionEnabledValue &&
+                bool.TryParse(extensionEnabledValue, out var extensionEnabled) &&
+                extensionEnabled))
             {
-                return true;
+                return Environment.GetEnvironmentVariable("APPCONFIG_ENDPOINT");
             }
 
-            if (Environment.GetEnvironmentVariable("APPCONFIG_EXTENSION_ENABLED") is string extensionEnabled)
-            {
-                return extensionEnabled.Equals("True", StringComparison.InvariantCultureIgnoreCase);
-            }
-
-            return false;
+            return null;
         }
 
         static void ChangeAppConfigurationSourcePriority(IList<IConfigurationSource> configSources)
@@ -63,5 +68,15 @@ public class AppConfigurationHostStartup : IWebJobsConfigurationStartup
             configSources.RemoveAt(configSources.Count - 1);
             configSources.Insert(configSources.Count - 1, appConfigSource);
         }
+
+        static bool IsDevelopment()
+        {
+            return Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "Development";
+        }
+    }
+
+    public void Configure(IWebJobsBuilder builder)
+    {
+        builder.Services.AddSingleton<INameResolver, ImprovedNameResolver>();
     }
 }
---
+++ src/AzureFunctions.Worker.Extensions.AppConfiguration.Host/ImprovedNameResolver.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Azure.WebJobs;
+
+/// <summary>
+/// Improves existing name resolver with ability to resolve nested settings
+/// </summary>
+internal sealed class ImprovedNameResolver : DefaultNameResolver
+{
+    private readonly IConfiguration configuration;
+
+    public ImprovedNameResolver(IConfiguration configuration)
+        : base(configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public override string Resolve(string name)
+    {
+        return this.configuration.GetConnectionStringOrSetting(name) ?? base.Resolve(name);
+    }
+}
